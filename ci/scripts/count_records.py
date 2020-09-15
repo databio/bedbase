ea5ff101c0b0c82e702e2f9943b1ee88bbd6f4b1
Michal Stolarczyk <stolarczyk.michal93@gmail.com>
2020-09-15 14:15:15 -0400

    use cofig getter

--- ci/scripts/count_records.py
+++ ci/scripts/count_records.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from argparse import ArgumentParser
-from bbconf import BedBaseConf
+from bbconf import BedBaseConf, get_bedbase_config
 from bbconf.const import *
 
 parser = ArgumentParser(description="Count records in a PostgreSQL table and verify")
@@ -13,7 +13,7 @@ parser.add_argument("-e", "--expected-count", help="Expected number of records",
 
 args = parser.parse_args()
 
-bbc = BedBaseConf('$GITHUB_WORKSPACE/ci/cfg/config_min.yaml')
+bbc = BedBaseConf(get_bedbase_config('$GITHUB_WORKSPACE/ci/cfg/config_min.yaml'))
 row_count = bbc._count_rows(table_name=args.table)
 if args.expected_count:
 	assert row_count == args.expected_count, "Number of records in the '{}' table ({}) not equal {}".format(args.table, row_count, args.expected_count)
