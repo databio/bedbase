71c3b6a25da317fdc8ba4d5de906488d3292065b
Oleksandr <sasha99250@gmail.com>
2023-08-11 07:49:28 -0400

    Update looper_config_bedboss.py

--- tutorial_files/bedboss/looper_config_bedboss.py
+++ tutorial_files/bedboss/looper_config_bedboss.py
@@ -1,4 +1,4 @@
 pep_config: ./bedboss_pep_config.yaml
 output_dir: $BEDBASE_DATA_PATH_HOST/outputs/outputs/bedstat_output/bedstat_pipeline_logs
 pipeline_interfaces:
-  sample:  ../pipeline_interface.yaml
+  sample:  ./pipeline_interface.yaml
