3aad9d38e4ced2a2d87589c081d34aaaf74fb432
Khoroshevskyi <sasha99250@gmail.com>
2023-08-14 09:30:32 -0400

    Updated looper config

--- tutorial_files/bedboss/looper_config_bedboss.py
+++
@@ -1,4 +0,0 @@
-pep_config: ./bedboss_pep_config.yaml
-output_dir: $BEDBASE_DATA_PATH_HOST/outputs/outputs/bedstat_output/bedstat_pipeline_logs
-pipeline_interfaces:
-  sample:  ./pipeline_interface.yaml
